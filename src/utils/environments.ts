7525f137b0cfa9775fd629a3e7457dd030871ffe
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2025-08-19 09:16:51 +0200

    fix: remove rtnotz from current environment preset

--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -4,29 +4,7 @@ import { CURRENT_CYCLE_POSITION, RETURN_TO_CYCLES } from "./constants";
 export const CAMPAIGN_PLAYALONG_PROJECT_ID =
   "5b6a1f95-73d1-4059-8af2-b9a645efd625";
 
-const CORE_PACKS = [
-  "core",
-  "rcore",
-  "rtnotz",
-  "nat",
-  "har",
-  "win",
-  "jac",
-  "ste",
-];
-
-export function campaignPlayalongPacks(cycle: string) {
-  const packs = [...CORE_PACKS];
-  if (cycle !== "core") {
-    packs.push(`${cycle}p`);
-  }
-
-  if (RETURN_TO_CYCLES[cycle]) {
-    packs.push(RETURN_TO_CYCLES[cycle]);
-  }
-
-  return packs;
-}
+const CORE_PACKS = ["core", "rcore", "nat", "har", "win", "jac", "ste"];
 
 export function currentEnvironmentPacks(cycles: Cycle[]) {
   const packs = [...CORE_PACKS];
@@ -38,10 +16,6 @@ export function currentEnvironmentPacks(cycles: Cycle[]) {
     if (cycle.code !== "core") {
       packs.push(`${cycle.code}p`);
     }
-
-    if (RETURN_TO_CYCLES[cycle.code]) {
-      packs.push(RETURN_TO_CYCLES[cycle.code]);
-    }
   }
 
   return packs;
@@ -49,6 +23,7 @@ export function currentEnvironmentPacks(cycles: Cycle[]) {
 
 export function limitedEnvironmentPacks(cycles: Cycle[]) {
   const packs = [...CORE_PACKS];
+  packs.push("rtnotz");
 
   for (const cycle of cycles) {
     if (cycle.code !== "core") {
@@ -58,6 +33,21 @@ export function limitedEnvironmentPacks(cycles: Cycle[]) {
     if (RETURN_TO_CYCLES[cycle.code]) {
       packs.push(RETURN_TO_CYCLES[cycle.code]);
     }
+  }
+
+  return packs;
+}
+
+export function campaignPlayalongPacks(cycle: string) {
+  const packs = [...CORE_PACKS];
+  packs.push("rtnotz");
+
+  if (cycle !== "core") {
+    packs.push(`${cycle}p`);
+  }
+
+  if (RETURN_TO_CYCLES[cycle]) {
+    packs.push(RETURN_TO_CYCLES[cycle]);
   }
 
   return packs;
